ea31c4b867708033cd77a5826105c77316a1900d
Amriteya Pandey <amriteyapandey@gmail.com>
2016-04-01 19:05:34 +0530

    Update main.py

--- image scraper/New folder/images/New folder/main.py
+++ image scraper/New folder/images/New folder/main.py
@@ -3,6 +3,3 @@ import urllib
 for count in range(38,70):
 	urllib.urlretrieve(""+str(count)+".jpg",str(count)+".jpg")
 	print count
-
-
-# http://viralboom.com/wp-content/uploads/2015/11/img561-17700.gif
